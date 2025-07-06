84a97c8f2639e900c03d039e924cd90e2db4ac0f
Eva De Haro <evadeharomunoz@gmail.com>
2025-07-06 19:45:39 +0200

    fix: add typo sizes

--- src/lib/tailwind.ts
+++ src/lib/tailwind.ts
@@ -1,15 +1,25 @@
-import plugin from "tailwindcss/plugin"
+import plugin from "tailwindcss/plugin";
 
 export const pluginTypography = plugin(({ addComponents }) => {
   addComponents({
     // desktop styles
-    ".text-header-xxl-desktop": {
-      "@apply text-[5rem] leading-[5rem] font-bold": {}
+    ".text-title-xl": {
+      "@apply text-[5rem] leading-[5rem] font-bold": {},
     },
-
-    // mobile styles
-    ".text-header-xxl": {
-      "@apply text-[2.25rem] leading-[2.25rem] font-bold": {}
+    ".text-title-l": {
+      "@apply text-[5rem] leading-[5rem] font-bold": {},
     },
-  })
-})
+    ".text-title-m": {
+      "@apply text-[5rem] leading-[5rem] font-bold": {},
+    },
+    ".text-subtitle": {
+      "@apply text-[5rem] leading-[5rem] font-bold": {},
+    },
+    ".text-text-l": {
+      "@apply text-[5rem] leading-[5rem] font-bold": {},
+    },
+    ".text-text-m": {
+      "@apply text-[5rem] leading-[5rem] font-bold": {},
+    },
+  });
+});
