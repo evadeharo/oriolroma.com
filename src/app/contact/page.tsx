c23cf7d74b338c89989eac17e3ecdc42a6f8f7d8
Eva De Haro <evadeharomunoz@gmail.com>
2025-07-06 20:04:42 +0200

    feat: file organization and clean

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+    return (
+      <div className="p-6">
+        <h1 className="text-title-xl">Oriol Roma | Contact</h1>
+      </div>
+    );
+  }
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="p-6">
-      <h1 className="text-header-xxl-desktop">Oriol Roma</h1>
+      <h1 className="text-title-xl">Oriol Roma</h1>
     </div>
   );
 }
